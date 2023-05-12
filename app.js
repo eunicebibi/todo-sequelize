60e407298a949b467ad36ed63897596b646ffc80
eunicebibi <agjay410@gmail.com>
2023-05-12 14:50:41 +0800

    feat: add package.json and modify router

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const session = require('express-session')
 const passport = require('passport')
+const bodyParser = require('body-parser')// 引用 body-parser
+const flash = require('connect-flash')
 // 載入設定檔，要寫在 express-session 以後
 const usePassport = require('./config/passport')
 const routes = require('./routes')
@@ -12,6 +14,7 @@ const Todo = db.Todo
 const User = db.User
 const app = express()
 const PORT = 3000
+
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(session({
@@ -19,17 +22,24 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  // console.log(req.user) 可以觀察是誰登入了
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')//設定success, warning訊息
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
+
 require('./config/passport')
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
-usePassport(app)
+
 app.use(routes)
-
-
-
-
-
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
+const db = require('../../models')
 const User = db.User
 
 
