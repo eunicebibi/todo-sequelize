a317b7e60e5800be3889e45d6d087b526f150ddf
eunicebibi <agjay410@gmail.com>
2023-05-12 14:06:03 +0800

    feat : create detail, index.hbs, modify seeder

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  // 查詢多筆資料：要在 findAll({ raw: true, nest: true }) 直接傳入參數
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
 
 app.get('/users/login', (req, res) => {
@@ -30,14 +36,43 @@ app.get('/users/register', (req, res) => {
 
 app.post('/users/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+  // MySQL 會使用 WHERE 來加入條件語句，在 Sequelize 裡，就會變成在參數裡加上 where，可以根據我們指令的條件來尋找資料。
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 })
 
 app.get('/users/logout', (req, res) => {
   res.send('logout')
 })
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  // 在 Sequelize 裡面，用 id 查詢的方法是 findByPk
+  return Todo.findByPk(id)
+  // 查詢單筆資料：在 res.render 時在物件實例 todo 後串上 todo.toJSON()。
+  // 資料轉換成 plain object 的方法，只需要直接在傳入樣板前加上 toJSON()
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
 
 
 app.listen(PORT, () => {
--- seeders/20230512051804-default-data.js
+++ seeders/20230512051804-default-data.js
@@ -1,8 +1,41 @@
 'use strict';
+const bcrypt = require('bcryptjs')
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [{
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: bcrypt.hashSync(SEED_USER.password, bcrypt.genSaltSync(10), null),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {})
+    try {
+      const user = await queryInterface.bulkInsert('Users', [{
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: bcrypt.hashSync(SEED_USER.password, bcrypt.genSaltSync(10), null),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+
+      const todos = Array.from({ length: 10 }).map((_, i) => ({
+        name: `name-${i}`,
+        UserId: user,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }));
+
+      await queryInterface.bulkInsert('Todos', todos, {});
+    } catch (error) {
+      console.error('Error seeding data:', error);
+    }
     /**
      * Add seed commands here.
      *
@@ -15,6 +48,13 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
+    try {
+      await queryInterface.bulkDelete('Todos', null, {});
+      await queryInterface.bulkDelete('Users', null, {});
+    } catch (error) {
+      console.error('Error deleting data:', error);
+    }
+  
     /**
      * Add commands to revert seed here.
      *
